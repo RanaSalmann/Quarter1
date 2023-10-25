4865b3cf31eaa6f683107199738b8a9a535fddb8
Rana Salman <sqamar138@gmail.com>
2023-10-25 23:01:35 +0500

    Loop function

---
+++ loop.js
@@ -0,0 +1,5 @@
+"use strict";
+for (let index = 1; index <= 10; index++) {
+    let result = 2 * index;
+    console.log("2*" + index + "=", result);
+}
---
+++ loop.ts
@@ -0,0 +1,5 @@
+for (let index=1;index<=10;index++)
+{
+    let result=2*index;
+    console.log("2*"+index+"=", result)
+   }
